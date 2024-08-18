a2263ea27a6ff554cd44cc832d377143bd48f49d
Max Samarin <maxim.samarin@gmail.com>
2024-08-18 15:22:01 +0300

    Moved upwards to include unity project

---
+++ Assets/Test3.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ModifiedValues;
+
+public class Test3 : MonoBehaviour
+{
+	public List<int> Integers = new List<int>();
+	// public List<ModifiedFloat> Stats = new List<ModifiedFloat>();
+
+	// private void Awake()
+	// {
+	// 	Stats.Add(new ModifiedFloat(5));
+	// 	Stats.Add(new ModifiedFloat(5));
+	// 	Stats.Add(new ModifiedFloat(5));
+	// }
+}
